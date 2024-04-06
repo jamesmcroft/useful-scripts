6100637ee693c2710ae15f6175ea4f576a377e5d
James Croft <jamz_c@hotmail.co.uk>
2024-04-06 11:01:12 +0100

    Add scripts to merge a git branch into the current branch

---
+++ git/Merge-GitBranch.psm1
@@ -0,0 +1,23 @@
+Function Merge-WithGitBranch {
+    Param (
+        [string]$Branch
+    )
+
+    Get-AllGitRepos
+    $currentBranch = git rev-parse --abbrev-ref HEAD
+
+    if ($currentBranch -ne $Branch) {
+        git checkout $Branch
+    }
+
+    git pull
+
+    if ($currentBranch -ne $Branch) {
+        git checkout $currentBranch
+        git merge $Branch
+    }
+}
+
+Function Merge-WithGitMain {
+    Merge-WithGitBranch "main"
+}
