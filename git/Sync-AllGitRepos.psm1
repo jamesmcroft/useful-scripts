90214dabf5c6109acd08b65f788271230c2b47f7
James Croft <jamz_c@hotmail.co.uk>
2024-04-06 10:55:34 +0100

    Add script to fetch and pull all changes for Git repos in a folder recursively

---
+++ git/Sync-AllGitRepos.psm1
@@ -0,0 +1,15 @@
+Function Sync-AllGitRepos {
+    Get-AllGitRepos
+
+    Get-ChildItem -Recurse -Depth 2 -Force |
+    Where-Object { $_.Mode -match "h" -and $_.FullName -like "*\.git" } |
+    ForEach-Object {
+        $dir = Get-Item (Join-Path $_.FullName "../")
+        Push-Location $dir
+
+        "Pulling $($dir.Name)..."
+
+        git pull
+        Pop-Location
+    }
+}
