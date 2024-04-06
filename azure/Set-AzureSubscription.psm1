6cc410b5702f32fae68fbb821f414bc4dfd17cc5
James Croft <jamz_c@hotmail.co.uk>
2024-04-06 11:30:24 +0100

    Add script to set Azure subscription from object

---
+++ azure/Set-AzureSubscription.psm1
@@ -0,0 +1,26 @@
+<#
+ .Synopsis
+  Sets the Azure subscription to use for the current session using the Azure CLI.
+
+ .Description
+  This function sets the Azure subscription to use for the current session using
+  the Azure CLI. The Azure CLI must be installed and available in the PATH.
+
+ .Parameter AzureConfig
+  An object containing the subscription to set. The object must have a property
+  named 'subscription' that contains the subscription ID.
+
+ .Example
+   $AzureConfig = @{
+       subscription = '00000000-0000-0000-0000-000000000000'
+   }
+   Set-AzureSubscription $AzureConfig
+#>
+Function Set-AzureSubscription {
+    Param (
+        [PSCustomObject]$AzureConfig
+    )
+
+    Write-Host Setting subscription to $AzureConfig.subscription
+    az account set --subscription $AzureConfig.subscription
+}
