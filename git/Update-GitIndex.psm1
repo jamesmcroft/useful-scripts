5319c870c5e228f00c9544e3c453c5029f6b82db
James Croft <jamz_c@hotmail.co.uk>
2024-04-06 11:36:05 +0100

    Add script to update the Git index cache for all repositories in a folder iteratively

---
+++ git/Update-GitIndex.psm1
@@ -0,0 +1,23 @@
+<#
+ .Synopsis
+  Updates the Git index cache for all repositories in the current directory.
+
+ .Description
+  This function iterates through all repositories in the current directory and
+  updates the Git index cache for each repository.
+
+ .Example
+   Update-GitIndex
+#>
+Function Update-GitIndex {
+    Get-ChildItem -Recurse -Depth 2 -Force |
+    Where-Object { $_.Mode -match "h" -and $_.FullName -like "*\.git" } |
+    ForEach-Object {
+        $dir = Get-Item (Join-Path $_.FullName "../")
+        Push-Location $dir
+ 
+        Write-Host Updating $dir repo Git index cache
+        git rm -r --cached .
+        git add .
+    }
+}
